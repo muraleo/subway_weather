dd87bf7950c7382cfb8fc8801276c52aa1a6ff68
muraleo <muraleo80923@gmail.com>
2016-12-02 21:05:38 -0500

    weather data add DATE and TIME

--- data_clean/weather_clean.js
+++ data_clean/weather_clean.js
@@ -1,18 +1,16 @@
 conn = new Mongo();
 db = conn.getDB("mta_weather");
 
-db.weather.createIndex({'DATE':1},{unique:1, dropDups:1})
-
 db.weather.find().forEach(
-        function(e){db.weather.update({},{$rename:{"YR--MODAHRMN":"DATE"}},false,true);
-         });
+        function(e){db.weather.update({},{$rename:{"YR--MODAHRMN":"DATE"}},false,true);});
 
 db.weather.aggregate(
    [
-     { $project: {_id:1,"DATE":1,"DIR":1,"SPD":1,"VSB" : 10.0,"TEMP":1,"MAX":1,"MIN":1,"PCP24":1} },
+     { $project: {_id:1,"DATE":1,"DIR":1,"SPD":1,"VSB":1,"TEMP":1,"MAX":1,"MIN":1,"PCP24":1} },
      { $out: "weather_temp" }
    ]
 )
+
 db.weather_temp.find({'PCP24':"*****"}).forEach(
         function(e) {db.weather_temp.update({_id:e._id},
                     {$set:{'PCP24':0.0}});}
@@ -29,5 +27,13 @@ db.weather_temp.find({'PCP24':"0.00T*****"}).forEach(
                 );
 
 db.weather_temp.find().forEach(
-        function(e){db.weather_temp.update({_id:e._id}, {$set:{DATE:e.DATE.toString()}});
+        function(e){db.weather_temp.update({_id:e._id}, {$set:{DATE:""+e.DATE}});
          });
+
+db.weather_temp.find().forEach(
+    function(e){db.weather_temp.update({_id:e._id},
+                               {$set:{DATE:e.DATE.substr(0,4)+
+                                       "-"+e.DATE.substr(4,2)+
+                                       "-"+e.DATE.substr(6,2),
+                                      TIME:e.DATE.substr(8,2)+
+                                       ":00:00"}})});
